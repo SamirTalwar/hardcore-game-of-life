01ccea7411ef02a03a7c263fd60fc9e0ac8b77de
Peter Kofler <peter.kofler@code-cop.org>
2015-05-18 21:18:22 +0200

    "I don't know" - generalize to go into some direction for a solution

--- src/main/java/com/noodlesandwich/gameoflife/StreamedIoRepresentation.java
+++ src/main/java/com/noodlesandwich/gameoflife/StreamedIoRepresentation.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import static com.noodlesandwich.gameoflife.CellPositions.singleCellAt;
 import static com.noodlesandwich.gameoflife.CellPositions.twoCellsAt;
 import static com.noodlesandwich.gameoflife.MyUniverse.aUniverseWith;
-import static com.noodlesandwich.gameoflife.MyUniverse.emptyUniverse;
 
 public class StreamedIoRepresentation implements IoRepresentation {
     private static final char LIVING = 'x';
@@ -19,24 +18,31 @@ public class StreamedIoRepresentation implements IoRepresentation {
 
     @Override
     public Universe deserializeNextUniverse() throws IOException {
-        String line = reader.readLine();
+        String firstLine = reader.readLine(); // test list - add test for empty stream
+        String secondLine = reader.readLine();
 
         // two ideas: Samir needs to vary both lines to get more complex logic to see pattern,
         // also make building blocks smaller by refactoring to see it earlier
         // Peter wants to start over to use only singleCells and see direct connection of (x,y) and CellPosition.
 
-        if (line.charAt(0) == LIVING) {
+        if (firstLine.charAt(0) == LIVING) {
             return aUniverseWith(singleCellAt(0, 0));
         }
 
-        if (line.charAt(1) == LIVING) {
-            if (line.charAt(2) == LIVING) {
-                return aUniverseWith(twoCellsAt(1, 0).and(twoCellsAt(1, 1)));
-            } else {
-                return aUniverseWith(singleCellAt(1, 0).and(twoCellsAt(0, 1)));
+        CellPositions rowOneCells = CellPositions.nothing();
+        if (firstLine.charAt(1) == LIVING) {
+            CellPositions firstCell = singleCellAt(1, 0);
+            rowOneCells = rowOneCells.and(firstCell);
+
+            if (firstLine.charAt(2) == LIVING) {
+                rowOneCells = rowOneCells.and(singleCellAt(2, 0));
             }
+
+            CellPositions rowTwoCells = twoCellsAt(secondLine.indexOf(LIVING), 1);
+            return aUniverseWith(rowOneCells.and(rowTwoCells));
         }
-        return emptyUniverse();
+
+        return aUniverseWith(rowOneCells);
     }
 
 }
